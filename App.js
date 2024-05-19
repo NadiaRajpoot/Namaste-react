f0fc9e45408ffe48046807c5aab5f0ce00e8597b
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2024-05-19 08:19:05 -0700

    Episode-02

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id : "heading"} , "this ia an h1 tag created with core react");
+const heading1 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "this ia an h1 tag created with core react"
+);
+const heading2 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "this ia an h2 tag created with core react"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //nested react elements
-const circle = React.createElement("h1" , {id: "circle" , style : {color : "blue"}  } , " I am a circle");
-const rectangle= React.createElement("h1" , {id: "rectangle" , style : {color : "yellow"}}  , " I am a rectangle");
-const triangle= React.createElement("h1" , {id: "triangle" , style : {color : "green"} } , " I am a triangle");
-const container = React.createElement("div" , {id : "container" }, [rectangle , circle, triangle]);
+
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
 root.render(container);
